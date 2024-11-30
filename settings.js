ff93c11178b7b7c13643ffae468c753e590c5b8e
Antonayy <tonynassry46@gmail.com>
2024-11-30 21:40:50 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "+2073011927"
+global.ownername = "ANTON NIGGERS"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '+2073011927'  //creator number
+global.ownername = 'ANTON NIGGERS' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Tech-God\n\nContact: +201273011927'
 
 //console view/theme
 global.themeemoji = '🪀'
